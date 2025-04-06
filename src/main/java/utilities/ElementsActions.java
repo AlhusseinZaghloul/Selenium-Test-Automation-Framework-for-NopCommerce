8cca1549e8b989508a20df450ef2e8635344db5a
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-06 13:14:41 +0200

    Add ElementsActions and LogsUtils utility classes for enhanced Selenium interactions and logging

---
+++ src/main/java/utilities/ElementsActions.java
@@ -0,0 +1,51 @@
+package utilities;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class ElementsActions {
+
+    /**
+     * Sends keys to a specified web element after ensuring it's visible.
+     *
+     * @param driver  The WebDriver instance.
+     * @param locator The By locator of the target element.
+     * @param data    The string data to send to the element.
+     */
+    @Step("Sending data: '{data}' to element: {locator}")
+    public static void sendData(WebDriver driver, By locator, String data) {
+        Waits.waitForElementVisible(driver, locator);
+        driver.findElement(locator).sendKeys(data);
+        LogsUtils.info("Data entered: ", data, " in element: ", locator.toString());
+    }
+
+    /**
+     * Clicks on a specified web element after ensuring it's clickable.
+     *
+     * @param driver  The WebDriver instance.
+     * @param locator The By locator of the target element.
+     */
+    @Step("Clicking on element: {locator}")
+    public static void clicking(WebDriver driver, By locator) {
+        Waits.waitForElementClickable(driver, locator);
+        driver.findElement(locator).click();
+        LogsUtils.info("Clicked on element: ", locator.toString());
+    }
+
+    /**
+     * Retrieves a specified attribute's value from a web element after ensuring it's visible.
+     *
+     * @param driver    The WebDriver instance.
+     * @param locator   The By locator of the target element.
+     * @param attribute The name of the attribute to retrieve (e.g., "value", "href", "class").
+     * @return The value of the specified attribute as a String.
+     */
+    @Step("Getting attribute '{attribute}' from element: {locator}")
+    public static String getAttributeFromElement(WebDriver driver, By locator, String attribute) {
+        Waits.waitForElementVisible(driver, locator);
+        LogsUtils.info("Getting attribute: ", attribute, " from element: ", locator.toString());
+        return driver.findElement(locator).getDomAttribute(attribute);
+    }
+}
---
+++ src/main/java/utilities/LogsUtils.java
@@ -0,0 +1,52 @@
+package utilities;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Utility class for providing logging functionalities using Log4j 2.
+ * This class provides static methods to easily log messages at different severity levels.
+ */
+public class LogsUtils {
+    //Private constructor to prevent instantiation of this utility class.
+    private LogsUtils() {}
+
+    public static final String LOGS_PATH = "test-outputs/Logs";
+
+    /**
+     * Retrieves the Logger instance for the class that calls this method.
+     * This approach ensures that log messages are associated with the originating class,
+     * making it easier to trace the source of the logs.
+     *
+     * @return The Logger instance for the calling class.
+     */
+    private static Logger logger() {
+        // Get the logger for the class that calls this method
+        return LogManager.getLogger(Thread.currentThread().getStackTrace()[3].getClassName());
+    }
+
+    public static void trace(String... message) {
+        logger().trace(String.join(" ", message));
+    }
+
+    public static void debug(String... message) {
+        logger().debug(String.join(" ", message));
+    }
+
+    public static void info(String... message) {
+        logger().info(String.join(" ", message));
+    }
+
+    public static void warn(String... message) {
+        logger().warn(String.join(" ", message));
+    }
+
+    public static void error(String... message) {
+        logger().error(String.join(" ", message));
+    }
+
+    public static void fatal(String... message) {
+        logger().fatal(String.join(" ", message));
+    }
+
+}
